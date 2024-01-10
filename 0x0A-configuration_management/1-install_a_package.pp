70465f9fb1e86ae1d368b5492c143b40d947527d
Yousif Abozid <yousif.abozid@yahoo.com>
2024-01-10 03:04:50 +0200

    add 0x0A-configuration_management task 3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,9 @@
-# Using Puppet, install flask from pip3.
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+package { 'python3-pip':  # Ensure pip3 is installed
+  ensure => installed,
+}
+
+exec { 'install_flask':  # Execute the command to install Flask 2.1.0
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => '/usr/bin',
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',  # Ensure Flask version is 2.1.0
 }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,9 +1,5 @@
-package { 'python3-pip':  # Ensure pip3 is installed
-  ensure => installed,
-}
-
-exec { 'install_flask':  # Execute the command to install Flask 2.1.0
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => '/usr/bin',
-  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',  # Ensure Flask version is 2.1.0
+# Using Puppet, create a manifest that kills a process named killmenow.
+exec { 'kill':
+  command => 'pkill -f killmenow',
+  path    => ['/usr/bin', '/usr/sbin']
 }
